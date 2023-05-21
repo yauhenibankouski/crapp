214ae86a98edff5f9aa6581de655f4d881d87ea7
Yauheni <yauheni.bankouski@gmail.com>
2023-05-21 19:21:43 +0200

    new code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Generic syntax:
+  # verb "path", to: "controller#action"
+  root to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
